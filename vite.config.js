015dc2f02306af93ac92f19abe5615a100b6cc17
sameer2407 <satyamjadaun2001@gmail.com>
2024-03-09 15:53:59 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+export default defineConfig(() => {
   return {
     define: {
-      "process.env.REACT_APP_WEATHER_API_KEY": JSON.stringify(
-        env.REACT_APP_WEATHER_API_KEY
-      ),
-      "process.env.REACT_APP_LOCATION_API_KEY": JSON.stringify(
-        env.REACT_APP_LOCATION_API_KEY
-      ),
+      __APP_ENV__: process.env.WEATHER_API,
     },
-    plugins: [react()],
   };
 });
